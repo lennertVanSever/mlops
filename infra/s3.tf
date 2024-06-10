4b7dcbc81fa10d3e270f532e2142bc12a063eca8
Lennert <lennert_vansever@hotmail.be>
2024-06-10 16:37:33 +0200

    make name unique

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,9 +1,13 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "mlops-bucket"
-  acl    = "private"
+  bucket = "unique-mlops-bucket-123456"  # Name should be unique
 
   tags = {
     Name        = "MLOps bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
